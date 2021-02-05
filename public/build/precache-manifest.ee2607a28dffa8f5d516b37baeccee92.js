3156b2d6facb4cc868d452b2f7c2ae6249d46022
Dinesh Khanal <dinesh_khanal@hotmail.com>
2021-02-05 18:38:45 +0545

    change in public folder

---
+++ public/build/precache-manifest.ee2607a28dffa8f5d516b37baeccee92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab939891c50d4c298f3f5432ec96c998",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e7b45e17b9e106ff549",
+    "url": "/static/css/main.51c87cb6.chunk.css"
+  },
+  {
+    "revision": "55dc56cbd6d167148310",
+    "url": "/static/js/2.b55dc4f5.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.b55dc4f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7b45e17b9e106ff549",
+    "url": "/static/js/main.fade47bd.chunk.js"
+  },
+  {
+    "revision": "0170db89e7fbb87593e0",
+    "url": "/static/js/runtime-main.af5f6906.js"
+  }
+]);
